d6ba313aa5911df098bf8a5c9a7788fc687725da
ivordev01 <rosa.ivor1721@gmail.com>
2026-03-26 17:14:27 -0300

    feat: 26-03

--- app/index.tsx
+++ app/index.tsx
@@ -7,6 +7,9 @@ export default function Index() {
       <Link href="/user/1?nome=joao">
         <Text>Ir para ID 1</Text>
       </Link>
+      <Link href="/user/2?nome=sandro">
+        <Text>Ir para ID 2</Text>
+      </Link>
     </View>
   );
 }
